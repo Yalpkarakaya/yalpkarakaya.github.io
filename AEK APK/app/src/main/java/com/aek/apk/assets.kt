b8166bbcd18d9d7a17ebcc6da8328504d97b9dde
Yalpkarakaya <111427462+Yalpkarakaya@users.noreply.github.com>
2025-10-19 14:08:25 +0300

    Delete AEK APK directory

--- AEK APK/app/src/main/java/com/aek/apk/assets.kt
+++
@@ -1,8 +0,0 @@
-package com.aek.apk
-
-object Assets {
-    // Web varlıklarını doğrudan paketlemek için kopyalanacak liste (gerekirse Gradle task ile)
-    val images = listOf(
-        // örn: "icons/icon-144x144.png"
-    )
-}
